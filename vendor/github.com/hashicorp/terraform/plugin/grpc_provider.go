6f1dcffc71f4975c66a0718199631870699910b4
Brian Flad <bflad417@gmail.com>
2019-04-18 14:02:57 -0400

    deps: github.com/hashicorp/terraform@v0.12.0-beta2
    Updated via:
    
    ```
    go get github.com/hashicorp/terraform@v0.12.0-beta2
    go mod tidy
    go mod vendor
    ```
    
    Output from acceptance testing:
    
    ```
    $ make testacc
    ==> Checking that code complies with gofmt requirements...
    TF_ACC=1 go test $(go list ./... |grep -v 'vendor') -v  -timeout 120m
    ?   	github.com/terraform-providers/terraform-provider-http	[no test files]
    === RUN   TestDataSource_http200
    --- PASS: TestDataSource_http200 (0.03s)
    === RUN   TestDataSource_http404
    --- PASS: TestDataSource_http404 (0.01s)
    === RUN   TestDataSource_withHeaders200
    --- PASS: TestDataSource_withHeaders200 (0.02s)
    === RUN   TestDataSource_utf8
    --- PASS: TestDataSource_utf8 (0.02s)
    === RUN   TestDataSource_utf16
    --- PASS: TestDataSource_utf16 (0.01s)
    === RUN   TestDataSource_compileError
    --- PASS: TestDataSource_compileError (0.00s)
    === RUN   TestProvider
    --- PASS: TestProvider (0.00s)
    PASS
    ok  	github.com/terraform-providers/terraform-provider-http/http	1.656s
    ```

--- vendor/github.com/hashicorp/terraform/plugin/grpc_provider.go
+++ vendor/github.com/hashicorp/terraform/plugin/grpc_provider.go
@@ -119,7 +119,13 @@ func (p *GRPCProvider) GetSchema() (resp providers.GetSchemaResponse) {
 	resp.ResourceTypes = make(map[string]providers.Schema)
 	resp.DataSources = make(map[string]providers.Schema)
 
-	protoResp, err := p.client.GetSchema(p.ctx, new(proto.GetProviderSchema_Request))
+	// Some providers may generate quite large schemas, and the internal default
+	// grpc response size limit is 4MB. 64MB should cover most any use case, and
+	// if we get providers nearing that we may want to consider a finer-grained
+	// API to fetch individual resource schemas.
+	// Note: this option is marked as EXPERIMENTAL in the grpc API.
+	const maxRecvSize = 64 << 20
+	protoResp, err := p.client.GetSchema(p.ctx, new(proto.GetProviderSchema_Request), grpc.MaxRecvMsgSizeCallOption{MaxRecvMsgSize: maxRecvSize})
 	if err != nil {
 		resp.Diagnostics = resp.Diagnostics.Append(err)
 		return resp
@@ -185,7 +191,6 @@ func (p *GRPCProvider) PrepareProviderConfig(r providers.PrepareProviderConfigRe
 
 func (p *GRPCProvider) ValidateResourceTypeConfig(r providers.ValidateResourceTypeConfigRequest) (resp providers.ValidateResourceTypeConfigResponse) {
 	log.Printf("[TRACE] GRPCProvider: ValidateResourceTypeConfig")
-
 	resourceSchema := p.getResourceSchema(r.TypeName)
 
 	mp, err := msgpack.Marshal(r.Config, resourceSchema.Block.ImpliedType())
@@ -402,6 +407,8 @@ func (p *GRPCProvider) PlanResourceChange(r providers.PlanResourceChangeRequest)
 
 	resp.PlannedPrivate = protoResp.PlannedPrivate
 
+	resp.LegacyTypeSystem = protoResp.LegacyTypeSystem
+
 	return resp
 }
 
@@ -453,13 +460,13 @@ func (p *GRPCProvider) ApplyResourceChange(r providers.ApplyResourceChangeReques
 	}
 	resp.NewState = state
 
+	resp.LegacyTypeSystem = protoResp.LegacyTypeSystem
+
 	return resp
 }
 
 func (p *GRPCProvider) ImportResourceState(r providers.ImportResourceStateRequest) (resp providers.ImportResourceStateResponse) {
 	log.Printf("[TRACE] GRPCProvider: ImportResourceState")
-
-	resSchema := p.getResourceSchema(r.TypeName)
 
 	protoReq := &proto.ImportResourceState_Request{
 		TypeName: r.TypeName,
@@ -479,6 +486,7 @@ func (p *GRPCProvider) ImportResourceState(r providers.ImportResourceStateReques
 			Private:  imported.Private,
 		}
 
+		resSchema := p.getResourceSchema(resource.TypeName)
 		state := cty.NullVal(resSchema.Block.ImpliedType())
 		if imported.State != nil {
 			state, err = msgpack.Unmarshal(imported.State.Msgpack, resSchema.Block.ImpliedType())
