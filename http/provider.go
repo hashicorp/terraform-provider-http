ffd27eaa3328f0caf899ae2ed34befa3b1a33ce2
Katy Moe <katy@katy.moe>
2020-10-02 19:16:41 +0100

    SDK v2.0.3

--- http/provider.go
+++ http/provider.go
@@ -1,11 +1,10 @@
 package http
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-func Provider() terraform.ResourceProvider {
+func Provider() *schema.Provider {
 	return &schema.Provider{
 		Schema: map[string]*schema.Schema{},
 
--- http/provider_test.go
+++ http/provider_test.go
@@ -3,23 +3,15 @@ package http
 import (
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/acctest"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-var testProviders = map[string]terraform.ResourceProvider{
+var testProviders = map[string]*schema.Provider{
 	"http": Provider(),
 }
 
 func TestProvider(t *testing.T) {
-	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
+	if err := Provider().InternalValidate(); err != nil {
 		t.Fatalf("err: %s", err)
 	}
-}
-
-func TestMain(m *testing.M) {
-	acctest.UseBinaryDriver("http", Provider)
-	resource.TestMain(m)
 }
