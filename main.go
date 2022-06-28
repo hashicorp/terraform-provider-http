58ca95cac11d2ce4e3484a3d73d8cc837f46540e
Benjamin Bennett <ben.bennett@hashicorp.com>
2022-06-28 14:12:30 +0100

    Adding support for debug flag (#93)

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+
 	"github.com/terraform-providers/terraform-provider-http/internal/provider"
 )
 
@@ -19,8 +21,14 @@ import (
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
 func main() {
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
 	err := providerserver.Serve(context.Background(), provider.New, providerserver.ServeOpts{
 		Address: "registry.terraform.io/hashicorp/http",
+		Debug:   debug,
 	})
 	if err != nil {
 		log.Fatal(err)
