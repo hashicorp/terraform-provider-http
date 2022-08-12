69ca9848ff89a53d80b19acf804e2c1f1f2e8c09
Benjamin Bennett <ben.bennett@hashicorp.com>
2022-08-12 10:12:05 +0100

    Code updates for terraform-plugin-framework v0.11.0

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -4,30 +4,31 @@ import (
 	"context"
 
 	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 )
 
-func New() tfsdk.Provider {
-	return &provider{}
+func New() provider.Provider {
+	return &httpProvider{}
 }
 
-var _ tfsdk.Provider = (*provider)(nil)
+var _ provider.Provider = (*httpProvider)(nil)
 
-type provider struct{}
+type httpProvider struct{}
 
-func (p *provider) GetSchema(context.Context) (tfsdk.Schema, diag.Diagnostics) {
+func (p *httpProvider) GetSchema(context.Context) (tfsdk.Schema, diag.Diagnostics) {
 	return tfsdk.Schema{}, nil
 }
 
-func (p *provider) Configure(context.Context, tfsdk.ConfigureProviderRequest, *tfsdk.ConfigureProviderResponse) {
+func (p *httpProvider) Configure(context.Context, provider.ConfigureRequest, *provider.ConfigureResponse) {
 }
 
-func (p *provider) GetResources(context.Context) (map[string]tfsdk.ResourceType, diag.Diagnostics) {
-	return map[string]tfsdk.ResourceType{}, nil
+func (p *httpProvider) GetResources(context.Context) (map[string]provider.ResourceType, diag.Diagnostics) {
+	return map[string]provider.ResourceType{}, nil
 }
 
-func (p *provider) GetDataSources(context.Context) (map[string]tfsdk.DataSourceType, diag.Diagnostics) {
-	return map[string]tfsdk.DataSourceType{
+func (p *httpProvider) GetDataSources(context.Context) (map[string]provider.DataSourceType, diag.Diagnostics) {
+	return map[string]provider.DataSourceType{
 		"http": &httpDataSourceType{},
 	}, nil
 }
