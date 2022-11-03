31abbf1b5c3ff8216a47a7e236cf551a9443c6e2
Benjamin Bennett <ben.bennett@hashicorp.com>
2022-11-03 10:49:51 +0000

    Removing unneeded files and moving test (#197)

--- internal/provider/data_source_http_test.go
+++ internal/provider/data_source_http_test.go
@@ -16,6 +16,77 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
+func TestDataSource_HTTPViaProxyWithEnv(t *testing.T) {
+	proxyRequests := 0
+	serverRequests := 0
+	pReqPtr := &proxyRequests
+	sReqPtr := &serverRequests
+
+	backend := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		*sReqPtr++
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	defer backend.Close()
+
+	backendURLStr := strings.Replace(backend.URL, "127.0.0.1", "backend", -1)
+	backendURL, err := url.Parse(backendURLStr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	proxy := func(u *url.URL) http.Handler {
+		pBackendURLStr := strings.Replace(u.String(), "backend", "127.0.0.1", -1)
+		pBackendURL, err := url.Parse(pBackendURLStr)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		p := httputil.NewSingleHostReverseProxy(pBackendURL)
+
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			*pReqPtr++
+			p.ServeHTTP(w, r)
+		})
+	}(backendURL)
+
+	frontend := httptest.NewServer(proxy)
+	defer frontend.Close()
+
+	t.Setenv("HTTP_PROXY", frontend.URL)
+	t.Setenv("HTTPS_PROXY", frontend.URL)
+
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV5ProviderFactories: protoV5ProviderFactories(),
+
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					data "http" "http_test" {
+						url = "%s"
+						insecure = "true"
+					}
+				`, backendURLStr),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.http.http_test", "status_code", "200"),
+					CheckServerAndProxyRequestCount(pReqPtr, sReqPtr),
+				),
+			},
+		},
+	})
+}
+
+func CheckServerAndProxyRequestCount(proxyRequestCount, serverRequestCount *int) resource.TestCheckFunc {
+	return func(_ *terraform.State) error {
+		if *proxyRequestCount != *serverRequestCount {
+			return fmt.Errorf("expected proxy and server request count to match: proxy was %d, while server was %d", *proxyRequestCount, *serverRequestCount)
+		}
+
+		return nil
+	}
+}
+
 func TestDataSource_200(t *testing.T) {
 	testHttpMock := setUpMockHttpServer(false)
 	defer testHttpMock.server.Close()
@@ -472,77 +543,6 @@ func TestDataSource_UnsupportedInsecureCaCert(t *testing.T) {
 			},
 		},
 	})
-}
-
-func TestDataSource_HTTPViaProxyWithEnv(t *testing.T) {
-	proxyRequests := 0
-	serverRequests := 0
-	pReqPtr := &proxyRequests
-	sReqPtr := &serverRequests
-
-	backend := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		*sReqPtr++
-		w.Header().Set("Content-Type", "text-plain")
-		w.WriteHeader(http.StatusOK)
-	}))
-
-	defer backend.Close()
-
-	backendURLStr := strings.Replace(backend.URL, "127.0.0.1", "backend", -1)
-	backendURL, err := url.Parse(backendURLStr)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	proxy := func(u *url.URL) http.Handler {
-		pBackendURLStr := strings.Replace(u.String(), "backend", "127.0.0.1", -1)
-		pBackendURL, err := url.Parse(pBackendURLStr)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		p := httputil.NewSingleHostReverseProxy(pBackendURL)
-
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			*pReqPtr++
-			p.ServeHTTP(w, r)
-		})
-	}(backendURL)
-
-	frontend := httptest.NewServer(proxy)
-	defer frontend.Close()
-
-	t.Setenv("HTTP_PROXY", frontend.URL)
-	t.Setenv("HTTPS_PROXY", frontend.URL)
-
-	resource.UnitTest(t, resource.TestCase{
-		ProtoV5ProviderFactories: protoV5ProviderFactories(),
-
-		Steps: []resource.TestStep{
-			{
-				Config: fmt.Sprintf(`
-					data "http" "http_test" {
-						url = "%s"
-						insecure = "true"
-					}
-				`, backendURLStr),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.http.http_test", "status_code", "200"),
-					CheckServerAndProxyRequestCount(pReqPtr, sReqPtr),
-				),
-			},
-		},
-	})
-}
-
-func CheckServerAndProxyRequestCount(proxyRequestCount, serverRequestCount *int) resource.TestCheckFunc {
-	return func(_ *terraform.State) error {
-		if *proxyRequestCount != *serverRequestCount {
-			return fmt.Errorf("expected proxy and server request count to match: proxy was %d, while server was %d", *proxyRequestCount, *serverRequestCount)
-		}
-
-		return nil
-	}
 }
 
 type TestHttpMock struct {
