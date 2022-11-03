015f8753ac463750be735fd008dd1cdf2a14af6c
Benjamin Bennett <ben.bennett@hashicorp.com>
2022-11-03 08:22:49 +0000

    Switching to using httputil single host reverse proxy for testing (#197)

--- internal/provider/data_source_http.go
+++ internal/provider/data_source_http.go
@@ -162,17 +162,13 @@ func (d *httpDataSource) Read(ctx context.Context, req datasource.ReadRequest, r
 
 	caCertificate := model.CaCertificate
 
-	//tr, ok := http.DefaultTransport.(*http.Transport)
-	//if !ok {
-	//	resp.Diagnostics.AddError(
-	//		"Error configuring http transport",
-	//		"Error http: Can't configure http transport.",
-	//	)
-	//	return
-	//}
-
-	tr := &http.Transport{
-		Proxy: http.ProxyFromEnvironment,
+	tr, ok := http.DefaultTransport.(*http.Transport)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Error configuring http transport",
+			"Error http: Can't configure http transport.",
+		)
+		return
 	}
 
 	tr.TLSClientConfig = &tls.Config{}
--- internal/provider/data_source_http_test.go
+++ internal/provider/data_source_http_test.go
@@ -6,13 +6,14 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/http/httputil"
+	"net/url"
 	"regexp"
 	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-
-	"github.com/terraform-providers/terraform-provider-http/internal/provider/testutils"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestDataSource_200(t *testing.T) {
@@ -474,24 +475,45 @@ func TestDataSource_UnsupportedInsecureCaCert(t *testing.T) {
 }
 
 func TestDataSource_HTTPViaProxyWithEnv(t *testing.T) {
-	server, err := testutils.NewHTTPServer()
+	proxyRequests := 0
+	serverRequests := 0
+	pReqPtr := &proxyRequests
+	sReqPtr := &serverRequests
+
+	backend := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		*sReqPtr++
+		w.Header().Set("Content-Type", "text-plain")
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	defer backend.Close()
+
+	backendURLStr := strings.Replace(backend.URL, "127.0.0.1", "backend", -1)
+	backendURL, err := url.Parse(backendURLStr)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	defer server.Close()
-	go server.ServeTLS()
+	proxy := func(u *url.URL) http.Handler {
+		pBackendURLStr := strings.Replace(u.String(), "backend", "127.0.0.1", -1)
+		pBackendURL, err := url.Parse(pBackendURLStr)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	proxy, err := testutils.NewHTTPProxyServer()
-	if err != nil {
-		t.Fatal(err)
-	}
+		p := httputil.NewSingleHostReverseProxy(pBackendURL)
 
-	defer proxy.Close()
-	go proxy.Serve()
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			*pReqPtr++
+			p.ServeHTTP(w, r)
+		})
+	}(backendURL)
 
-	t.Setenv("HTTP_PROXY", fmt.Sprintf("http://%s", proxy.Address()))
-	t.Setenv("HTTPS_PROXY", fmt.Sprintf("http://%s", proxy.Address()))
+	frontend := httptest.NewServer(proxy)
+	defer frontend.Close()
+
+	t.Setenv("HTTP_PROXY", frontend.URL)
+	t.Setenv("HTTPS_PROXY", frontend.URL)
 
 	resource.UnitTest(t, resource.TestCase{
 		ProtoV5ProviderFactories: protoV5ProviderFactories(),
@@ -500,17 +522,27 @@ func TestDataSource_HTTPViaProxyWithEnv(t *testing.T) {
 			{
 				Config: fmt.Sprintf(`
 					data "http" "http_test" {
-						url = "https://%s"
+						url = "%s"
 						insecure = "true"
 					}
-				`, server.Address()),
+				`, backendURLStr),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("data.http.http_test", "status_code", "200"),
-					testutils.TestCheckBothServerAndProxyWereUsed(server, proxy),
+					CheckServerAndProxyRequestCount(pReqPtr, sReqPtr),
 				),
 			},
 		},
 	})
+}
+
+func CheckServerAndProxyRequestCount(proxyRequestCount, serverRequestCount *int) resource.TestCheckFunc {
+	return func(_ *terraform.State) error {
+		if *proxyRequestCount != *serverRequestCount {
+			return fmt.Errorf("expected proxy and server request count to match: proxy was %d, while server was %d", *proxyRequestCount, *serverRequestCount)
+		}
+
+		return nil
+	}
 }
 
 type TestHttpMock struct {
