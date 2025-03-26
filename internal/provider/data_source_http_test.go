9e22b3f6beb97b60082afedcd7250ee1323807e6
Sheridan C Rawlins <scr@yahooinc.com>
2025-03-26 10:36:40 -0700

    and another place for the version checks.

--- internal/provider/data_source_http_test.go
+++ internal/provider/data_source_http_test.go
@@ -970,6 +970,10 @@ func TestDataSource_ResponseBodyText(t *testing.T) {
 
 	resource.ParallelTest(t, resource.TestCase{
 		ProtoV5ProviderFactories: protoV5ProviderFactories(),
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			//test fails in TF 0.14.x due to https://github.com/hashicorp/terraform-provider-http/issues/58
+			tfversion.SkipBetween(tfversion.Version0_14_0, tfversion.Version0_15_0),
+		},
 		Steps: []resource.TestStep{
 			{
 				Config: fmt.Sprintf(`
