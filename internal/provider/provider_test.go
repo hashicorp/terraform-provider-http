1f0d5a70b95a8d1557d35bbef6df1dc4424ecffa
Austin Valle <austinvalle@gmail.com>
2026-02-26 14:16:46 -0500

    first draft + todos

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -5,12 +5,12 @@ package provider
 
 import (
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 )
 
 //nolint:unparam
-func protoV5ProviderFactories() map[string]func() (tfprotov5.ProviderServer, error) {
-	return map[string]func() (tfprotov5.ProviderServer, error){
-		"http": providerserver.NewProtocol5WithError(New()),
+func protoV6ProviderFactories() map[string]func() (tfprotov6.ProviderServer, error) {
+	return map[string]func() (tfprotov6.ProviderServer, error){
+		"http": providerserver.NewProtocol6WithError(New()),
 	}
 }
